baf802b4c69d85ff28d22c9c8e35eb58d245ecaa
Brian Andreasen <bspinnfade@gmail.com>
2018-01-16 14:31:51 -0600

    Getting Data from REST API
    Granted it doesn't look pretty I have data coming in from the REST API
    to a simple text view. The data is not parsed and is coming in as a
    giant string which was to be expected.
    
    I will be implementing the rest of the get other non parameterized
    methods we will need.
    
    We will want to refactor later on to remove the code duplication that
    will likely be occuring.

--- app/src/main/java/com/code4socialgood/code4socialgood/MainActivity.java
+++ app/src/main/java/com/code4socialgood/code4socialgood/MainActivity.java
@@ -1,13 +1,70 @@
 package com.code4socialgood.code4socialgood;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import com.code4socialgood.code4socialgood.utilities.NetworkUtils;
+import java.io.IOException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mGetProjectsDataTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mGetProjectsDataTextView = (TextView) findViewById(R.id.tv_getProjectsJSON);
+    }
+
+    private void getTheProjectsData(){
+        String projectsQuery = "http://dev-api.code4socialgood.org/api/projects";
+        URL projectsURL = NetworkUtils.buildURL(projectsQuery);
+        mGetProjectsDataTextView.setText(projectsURL.toString());
+        new GetProjectsTask().execute(projectsURL);
+    }
+
+    public class GetProjectsTask extends AsyncTask<URL,Void, String>{
+
+        @Override
+        protected String doInBackground(URL... urls) {
+            URL projectsURL = urls[0];
+            String projectsResults = null;
+            try{
+                projectsResults = NetworkUtils.getResponseFromHttpUrl(projectsURL);
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+            return projectsResults;
+        }
+
+        //Not Implemented Yet
+        @Override
+        protected void onPostExecute(String s) {
+            if(s != null && !s.equals("")){
+                mGetProjectsDataTextView.setText(s);
+            }
+        }
+    }
+
+    //Not yet implemented
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    //Not yet implemented
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemClicked = item.getItemId();
+        if(itemClicked == item.getItemId()){
+            getTheProjectsData();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/code4socialgood/code4socialgood/utilities/NetworkUtils.java
+++ app/src/main/java/com/code4socialgood/code4socialgood/utilities/NetworkUtils.java
@@ -13,17 +13,63 @@ import java.net.URL;
 import java.util.Scanner;
 
 
-public final class NetworkUtils {
+public class NetworkUtils {
 
     //REST API URLs
-    private static final String devBaseURL = "http://dev-api.code4socialgood.org/api/";
+
+    //PROJECT URLS
+    final static String devBaseURL = "http://dev-api.code4socialgood.org/api/";
     //private static final String prodBaseURL = "";
 
+    final static String projectsURL = devBaseURL + "projects/";
+    final static String projApplications = projectsURL + "applications";
+    final static String projHeroes = projectsURL + "heroes";
+    final static String projOrganization = projectsURL + "organization";
+    final static String projSearch = projectsURL + "search";
+    final static String projUser = projectsURL + "user";
+
+    //Organization URLs
+    final static String orgsURL = devBaseURL + "organizations/";
+    final static String orgCountriesTotal = orgsURL + "countries/ total";
+    final static String orgSearch = orgsURL + "search/";
+
+    //Volunteers URLs
 
 
+    //Builds a projects URL to test getting data.
+    public static URL buildURL(String projectsURL){
+        Uri builtUri = Uri.parse(projectsURL);
 
+        URL url = null;
+        try{
+            url = new URL(builtUri.toString());
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+        return url;
+    }
 
-    //method for response returned from HttpUrl
+//method for response returned from HttpUrl
+    public static String getResponseFromHttpUrl(URL url) throws IOException{
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try{
+            InputStream inStream = urlConnection.getInputStream();
+            Scanner scanner = new Scanner(inStream);
+            scanner.useDelimiter("//A");
 
-
+            boolean hasInput = scanner.hasNext();
+            if(hasInput){
+                return scanner.next();
+            }
+            else{
+                return null;
+            }
+        }
+        finally{
+            urlConnection.disconnect();
+        }
+    }
 }
+
+
+
