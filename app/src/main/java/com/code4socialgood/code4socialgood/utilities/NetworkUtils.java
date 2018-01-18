d12b64be1d809f35d7853e2d08add17065ff3a56
Brian Andreasen <bspinnfade@gmail.com>
2018-01-18 00:51:55 -0600

    Introuced basic error handling for string input on get data URL

--- app/src/main/java/com/code4socialgood/code4socialgood/utilities/NetworkUtils.java
+++ app/src/main/java/com/code4socialgood/code4socialgood/utilities/NetworkUtils.java
@@ -8,37 +8,14 @@ import android.net.Uri;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Scanner;
 
 
 public class NetworkUtils {
 
-    //REST API URLs
-
-    //PROJECT URLS
-    final static String devBaseURL = "http://dev-api.code4socialgood.org/api/";
-    //private static final String prodBaseURL = "";
-
-    final static String projectsURL = devBaseURL + "projects/";
-    final static String projApplications = projectsURL + "applications";
-    final static String projHeroes = projectsURL + "heroes";
-    final static String projOrganization = projectsURL + "organization";
-    final static String projSearch = projectsURL + "search";
-    final static String projUser = projectsURL + "user";
-
-    //Organization URLs
-    final static String orgsURL = devBaseURL + "organizations/";
-    final static String orgCountriesTotal = orgsURL + "countries/ total";
-    final static String orgSearch = orgsURL + "search/";
-
-    //Volunteers URLs
-
-
-    //Builds a projects URL to test getting data.
-    public static URL buildURL(String projectsURL){
-        Uri builtUri = Uri.parse(projectsURL);
+    public static URL buildURL(String urlString){
+        Uri builtUri = Uri.parse(urlString);
 
         URL url = null;
         try{
