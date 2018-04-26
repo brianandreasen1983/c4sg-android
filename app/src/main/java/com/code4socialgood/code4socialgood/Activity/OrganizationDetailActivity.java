b19994f77295e8d5e424f555b162c31959760be7
joshisayali <sayali.j23@gmail.com>
2018-04-26 16:52:40 -0700

    Added new activity for organization detail

---
+++ app/src/main/java/com/code4socialgood/code4socialgood/Activity/OrganizationDetailActivity.java
@@ -0,0 +1,15 @@
+package com.code4socialgood.code4socialgood.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.code4socialgood.code4socialgood.R;
+
+public class OrganizationDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_organization_detail);
+    }
+}
